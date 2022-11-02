ac6cc9ce111ed4302cf640395c157f2e2107cad6
JeremyTanudjaja <79756347+JeremyTanudjaja@users.noreply.github.com>
2022-11-02 23:58:28 +0700

    Huge Update
    Added AJAX and HTML code, Admin Login using hardcoded data

---
+++ logic/PageTransitionController.py
@@ -0,0 +1,13 @@
+
+class Transition_Controller:
+
+    def __init__(self):
+        print("Transition Controller Called")
+
+    def login_admin(self, admin_data):
+        admin_name = admin_data['name']
+        admin_password = admin_data['password']
+        if admin_name.lower() == "admin" and admin_password == "12345678":
+            return True
+        print("Login Failed")
+        return False
--- logic/routes.py
+++ logic/routes.py
@@ -1,14 +1,61 @@
-from flask import render_template, url_for, flash, redirect, session, app
-from logic.controller import Controller_Test
+from flask import render_template, url_for, flash, redirect, session, app, request
+from logic.PageTransitionController import Transition_Controller
 from logic import app
 
-test = Controller_Test()
 
-@app.route("/home")
-@app.route("/")
+page_controller = Transition_Controller()
+admin_data = None
+
+@app.route("/", methods=['POST','GET'])
 def index():
-    test_data = test.grab_data()
-    return render_template("index.html", employees=test_data)
+    if request.method == "POST":
+        request_data = request.form
+        print(f"Name:{request_data['name']}\nPass:{request_data['password']}")
+        if page_controller.login_admin(request_data):
+            global admin_data
+            admin_data = {'ID': 1001,
+                          'name': request_data['name']}
+            print(admin_data)
+            return redirect("/home")
+    return render_template("index.html")
 
+@app.route("/home", methods=['POST','GET'])
 def home():
-    return render_template("home.html")
+    print(admin_data)
+    return render_template("home.html", admin_data = admin_data)
+
+@app.route("/Departments", methods = ['POST','GET'])
+def departments():
+    return render_template("page_init/departments_init.html")
+
+@app.route("/Employees", methods = ['POST','GET'])
+def employees():
+    return render_template("page_init/employee_init.html")
+
+@app.route("/Product", methods = ['POST','GET'])
+def products():
+    return render_template("page_init/products_init.html")
+
+@app.route("/Inventories", methods = ['POST','GET'])
+def inventories():
+    return render_template("page_init/inventories_init.html")
+
+@app.route("/Vendor", methods = ['POST','GET'])
+def vendors():
+    return render_template("page_init/vendor_init.html")
+
+@app.route("/Material", methods = ['POST','GET'])
+def materials():
+    return render_template("page_init/materials_init.html")
+
+@app.route("/Material_Order", methods = ['POST','GET'])
+def material_orders():
+    return render_template("page_init/material_order_init.html")
+
+@app.route("/Customer", methods = ['POST','GET'])
+def customers():
+    return render_template("page_init/customer_init.html")
+
+@app.route("/Sales", methods = ['POST','GET'])
+def sales_product():
+    return render_template("page_init/sales_product_init.html")
